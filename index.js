61764821136f6fe3d42173c7f5bb70fa7d256355
Clicia Scarlet <yuyuvn@mac.com>
2019-04-18 00:39:35 +0900

    Add metrics

--- index.js
+++ index.js
@@ -6,32 +6,54 @@ const jsonParser = bodyParser.json()
 const Chicken = require('./morninAPI/gatt');
 
 let chicken;
+chicken = new Chicken(process.env.CHICKEN_TOKEN);
 
-SUPPORTED_COMMANDS = ['open', 'close', 'stop', 'hightSpeedOpen', 'hightSpeedClose']
+SUPPORTED_COMMANDS = ['open', 'close', 'stop', 'hightSpeedOpen', 'hightSpeedClose', 'disconnect']
 
-async function connect(token) {
-  chicken = new Chicken(token);
-  try {
-    return chicken.connect();
-  } catch (err) {
-    if (!(err instanceof UnhandledPromiseRejectionWarning)) throw err;
+async function connect() {
+  if (chicken.connectedPeripheral && chicken.connectedPeripheral.state == 'connected') {
+    return;
   }
+
+  await chicken.connect();
+}
+
+async function wait(timeNs) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, timeNs)
+  })
 }
 
 SUPPORTED_COMMANDS.forEach((command) => {
-  app.post(`/act/${command}`, jsonParser, async (req, res) => {
+  app.post(`/act/${command}`, jsonParser, async (req, res, next) => {
     try {
-      console.log("Received command: ", command)
       const token = req.body.token;
+
+      if (token != process.env.CHICKEN_TOKEN) throw 'Wrong token';
+
+      console.log("Received command: ", command)
       await connect(token);
   
-      chicken[command]();
+      await chicken[command]();
       res.send("Done!")
     } catch(e) {
-      console.error(e)
-      res.send(e)
+      next(e)
     }
   })
 })
 
+app.get('/metrics', async (req, res, next) => {
+  try {
+    console.log("Scraping metrics");
+    await connect();
+    const batteryStatus = await chicken.battery();
+    output = `# HELP chicken_battery Robot battery level
+# TYPE chicken_battery gauge
+chicken_battery{} ${batteryStatus}
+`;
+    res.send(output);
+  } catch(e) {
+    next(e)
+  }
+})
 app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`))
